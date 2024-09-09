b54c965c65d14642f3c9a75b4fb769ffc9100438
jan.sattler <jan.sattler@infis.cz>
2024-09-09 13:08:40 +0200

    b

--- src/test/test.java
+++ src/test/test.java
@@ -1,7 +1,36 @@
 package test;
 
+import fileworks.DataExport;
+import fileworks.DataImport;
+
 public class test {
     public static void main(String[] args) {
-        System.out.println("kok1");
+        DataImport di = new DataImport("inputs.txt");
+
+        String line;
+        String[] params;
+        int a, b;
+        while (di.hasNext()) {
+            line = di.readLine();
+            System.out.println(line);
+            params = line.split(",");
+            a = Integer.parseInt(params[0]);
+            b = Integer.parseInt(params[1]);
+            switch (params[2]) {
+                case "A":
+                    System.out.println(a+b); break;
+
+                case "S":
+                    System.out.println(a-b); break;
+
+                case "M":
+                    System.out.println(a*b); break;
+
+                case "D":
+                    System.out.println(a/b); break;
+            }
+        }
+        //di.printFile();
+        di.finishImport();
     }
 }
