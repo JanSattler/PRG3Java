45bb7cf882524dd39c83e692f968732ca6db608e
jan.sattler <jan.sattler@infis.cz>
2024-12-09 13:20:00 +0100

    b

---
+++ src/fileworks/Ukol.java
@@ -0,0 +1,10 @@
+package fileworks;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+
+public class Ukol {
+    public static void main(String[] args) {
+    }
+}
