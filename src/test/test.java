603171cd6702a2de1cecf8721947c077935ada67
Jan Sattler <jan.sattler@infis.cz>
2024-12-19 13:41:56 +0100

    test

--- src/test/test.java
+++ src/test/test.java
@@ -7,6 +7,9 @@ public class test {
     public static void main(String[] args) {
         DataImport di = new DataImport("inputs.txt");
         DataExport de = new DataExport("outputs.txt");
+        System.out.println("čus asasdasdasa");
+        int kok = 2;
+        System.o
 
         String line;
         String[] params;
