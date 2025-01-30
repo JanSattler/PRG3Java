283afd070f66134e17114b6a9a920e197638520f
jan.sattler <jan.sattler@infis.cz>
2025-01-30 10:08:22 +0100

    b

---
+++ src/Time/TimePractice.java
@@ -0,0 +1,10 @@
+package Time;
+
+import java.time.LocalTime;
+
+public class TimePractice {
+    public static void main(String[] args) {
+        LocalTime time = LocalTime.now();
+        System.out.println(time);
+    }
+}
---
+++ src/exceptionsBruh/Trying.java
@@ -0,0 +1,12 @@
+package exceptionsBruh;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.PrintWriter;
+
+public class Trying {
+    public static void main(String[] args) {
+        //vytvoř soubor, pridej do nej 10 cisel
+    }
+}
