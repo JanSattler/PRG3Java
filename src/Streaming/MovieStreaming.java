f69add8c0f650aa8aec769cd9f42277d3cb185ab
jan.sattler <jan.sattler@infis.cz>
2025-03-05 10:33:30 +0100

    b

--- src/Streaming/MovieStreaming.java
+++ src/Streaming/MovieStreaming.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 
 public class MovieStreaming {
@@ -16,6 +17,25 @@ public class MovieStreaming {
                 params = line.split(";");
                 movies.add(new Movie(params[0], Integer.parseInt(params[1]), params[2], Double.parseDouble(params[3])));
             }
+
+            movies.stream()
+                    .filter(movie -> movie.getYear() > 2000)
+                    .forEach(System.out::println);
+
+            movies.stream()
+                    .sorted(Comparator.comparingDouble(Movie::getRating))
+                    .forEach(System.out::println);
+
+            double avgRating = movies.stream()
+                    .filter(movie -> movie.genre.equals("Horror"))
+                    .mapToDouble(movie -> movie.rating)
+                    .average()
+                    .orElse(0);
+
+            List<Movie> actionMovies = movies.stream()
+                    .filter(movie -> movie.genre.equals("Action") && movie.year > 2000)
+                    .distinct() //kdyby náhodou tam byl duplikát
+                    .toList();
         } catch (IOException e) {
             System.out.println("Nepodarilo se najit soubor");
             System.out.println(e.getMessage());
@@ -34,6 +54,22 @@ class Movie {
         this.year = year;
         this.genre = genre;
         this.rating = rating;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public double getRating() {
+        return rating;
     }
 
     @Override
