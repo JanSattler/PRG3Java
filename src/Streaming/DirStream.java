548e7766126f3778c932f88b45b793845025377e
jan.sattler <jan.sattler@infis.cz>
2025-03-27 09:52:36 +0100

    pico123

---
+++ src/Streaming/DirStream.java
@@ -0,0 +1,25 @@
+package Streaming;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.stream.Stream;
+
+public class DirStream {
+    public static void main(String[] args) {
+        //známý způsob
+        //File[] files = new File("cesta").listFiles();
+
+        Path start = Paths.get("inputs\\testData2");
+
+        try (Stream<Path> fileStream = Files.walk(start)) { //proč to tu je takhle?, vysvětlení try with resources
+            //Files.walk(start)   //vrací stream
+            fileStream
+                    .forEach(System.out::println);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
