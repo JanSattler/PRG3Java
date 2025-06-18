872d86b5221a092e85a2c399a6331a1865b5a700
jan.sattler <jan.sattler@infis.cz>
2025-06-18 10:31:44 +0200

    praáce na final

--- src/final2/PharmacyPractice.java
+++ src/final2/PharmacyPractice.java
@@ -1,21 +1,45 @@
 package final2;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class PharmacyPractice {
 
     static List<Medication> medications = new ArrayList<>();
     static List<Order> orders = new ArrayList<>();
 
+    static HashMap<String, Integer> stockMedications = new HashMap<>();
 
-    static boolean loadMedications(String filePath){
+
+    static void loadAndProcessOrders(String filePath) {
         try {
-            medications  = Files.lines(Paths.get(filePath))
+            Files.lines(Paths.get(filePath))
+                    .skip(1)
+                    .map(line -> line.split(";"))
+                    .map(params -> new Order(
+                            params[0],
+                            params[1],
+                            Integer.parseInt(params[2])
+                            )
+
+
+                    );
+
+        } catch (IOException e) {
+            System.out.println("soubor nenalezen");
+        }
+    }
+
+    static boolean loadMedications(String filePath) {
+        try {
+            medications = Files.lines(Paths.get(filePath))
                     .skip(1)
                     .map(line -> line.split(";"))
                     .map(params -> new Medication(
@@ -31,46 +55,53 @@ public class PharmacyPractice {
         }
     }
 
-    static void loadAndProcessOrders(String filePath){
-        try {
-            orders  = Files.lines(Paths.get(filePath))
-                    .skip(1)
-                    .map(line -> line.split(";"))
-                    .map(params -> new Order(
-                            params[0],
-                            params[1],
-                            Integer.parseInt(params[2])
-                    ))
-                    .toList();
-        } catch (IOException e) {
-            System.out.println("soubor nenalezen");
+    static void printInventory() {
+        for (String med : stockMedications.keySet()) {
+            System.out.println(med+": "+ stockMedications.get(med));
+            System.out.println(
+                    medications.stream()
+                            .collect(Collectors.groupingBy(Medication::getName, Collectors.summingDouble(Medication::getPricePerPiece))
+            ));
         }
-    }
-
-    static void printInventory(){
 
     }
 
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         loadMedications("inputs\\final2\\medications.csv");
         loadAndProcessOrders("inputs\\final2\\orders.csv");
 
-        HashMap<String, Integer> stockMedications = new HashMap<>();
 
         for (Order order : orders) {
-            if (!stockMedications.containsKey(order.getMedicationName())) {
-                ArrayList<Order> newMed = new ArrayList<>();
-                newMed.add(order);
-                stockMedications.put(order.getMedicationName(), order.getPieces());
-            } else {
-                stockMedications.get(order.getMedicationName()).add(order.getPieces());
+            BufferedWriter bw = new BufferedWriter(new FileWriter("outputs\\invalids.txt"));
+            ArrayList<String> medNames = new ArrayList<>();
+            for (Medication medication : medications) {
+                medNames.add(medication.getName());
             }
+
+            if (order.getAction().equals("restock")) {
+                if(stockMedications.containsKey(order.getMedicationName())) {
+                    stockMedications.replace(order.getMedicationName(), stockMedications.get(order.getMedicationName()), stockMedications.get(order.getMedicationName())+ order.getPieces());
+                } else {
+                    stockMedications.put(order.getMedicationName(), order.getPieces());
+                }
+            } else {
+                if (!medNames.contains(order.getMedicationName())) {
+                    bw.write(order.toString() + "LÉK NENALEZEN");
+                    bw.close();
+                } else if (stockMedications.get(order.getMedicationName()) < order.getPieces())  {
+                    bw.write(order.toString() + "NEDOSTATEK KUSŮ NA SKLADĚ");
+                    bw.newLine();
+                }
+            }
+
         }
 
     }
 
-} class Medication{
+}
+
+class Medication {
     String name, type;
     double pricePerPiece;
 
@@ -113,5 +144,14 @@ class Order {
 
     public int getPieces() {
         return pieces;
+    }
+
+    @Override
+    public String toString() {
+        return "Order{" +
+                "action='" + action + '\'' +
+                ", medicationName='" + medicationName + '\'' +
+                ", pieces=" + pieces +
+                '}';
     }
 }
